f911eb2fee905d544877f91d69d8ec0637c7a8dd
Sergio Martin <martiser@ethz.ch>
2021-04-17 17:22:09 +0200

    Fixing logging

--- source/SDLPopInstance.cc
+++ source/SDLPopInstance.cc
@@ -295,8 +295,8 @@ void SDLPopInstance::advanceFrame()
 void SDLPopInstance::printFrameInfo()
 {
  printf("[Jaffar]  + Action: %s\n", _currentMove.c_str());
- printf("[Jaffar]  + [Kid]   Room: %d, Pos.x: %3d, Pos.y: %3d, Frame: %3d, HP: %d/%d, Sequence: %4d\n", int(Kid->room), int(Kid->x), int(Kid->y), int(Kid->frame), int(Kid->curr_hp), int(Kid->max_hp), int(Kid->curr_seq));
- printf("[Jaffar]  + [Guard] Room: %d, Pos.x: %3d, Pos.y: %3d, Frame: %3d, HP: %d/%d, Sequence: %4d\n", int(Guard->room), int(Guard->x), int(Guard->y), int(Guard->frame), int(Guard->curr_hp), int(Guard->max_hp), int(Guard->curr_seq));
+ printf("[Jaffar]  + [Kid]   Room: %d, Pos.x: %3d, Pos.y: %3d, Frame: %3d, HP: %d/%d, Sequence: %4d\n", int(Kid->room), int(Kid->x), int(Kid->y), int(Kid->frame), int(*hitp_curr), int(*hitp_max), int(Kid->curr_seq));
+ printf("[Jaffar]  + [Guard] Room: %d, Pos.x: %3d, Pos.y: %3d, Frame: %3d, HP: %d/%d, Sequence: %4d\n", int(Guard->room), int(Guard->x), int(Guard->y), int(Guard->frame), int(*guardhp_curr), int(*guardhp_max), int(Guard->curr_seq));
  printf("[Jaffar]  + Exit Door Open: %s\n", isLevelExitDoorOpen() ? "Yes" : "No");
  printf("[Jaffar]  + RNG State: 0x%08X (Last Loose Tile Sound Id: %d)\n", *random_seed, *last_loose_sound);
 }
