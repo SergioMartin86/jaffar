3fc435dd1e0547909d4729206c1c860299598e74
Sergio Martin <martiser@ethz.ch>
2021-05-19 20:25:05 +0200

    More fixes

--- examples/run-lvl10.sh
+++ examples/run-lvl10.sh
@@ -2,4 +2,4 @@
 
 source .set_env.sh
 
-mpirun -n 1 jaffar-train --maxSteps 950 --savFile quicksave.sav jaffar.config 
+mpirun -n 1 jaffar-train --maxSteps 530 --savFile saves/lvl10.sav scripts/lvl10.jaffar 
