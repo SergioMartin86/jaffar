856a2c0256234b0bcd5774cd9a49671ded3cc095
Sergio Martin <martiser@ethz.ch>
2021-12-02 13:25:14 +0100

    Adding level12

--- source/frame.h
+++ source/frame.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define _MAX_FRAME_DIFF 145
-#define _MAX_RULE_COUNT 8
-#define _MAX_MOVELIST_SIZE 250
+#define _MAX_FRAME_DIFF 160
+#define _MAX_RULE_COUNT 11
+#define _MAX_MOVELIST_SIZE 610
 #define _MAX_MOVELIST_STORAGE ((_MAX_MOVELIST_SIZE/2) + 1)
 #define _FRAME_DATA_SIZE 2710
 
