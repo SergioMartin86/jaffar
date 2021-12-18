ce5cd297629fe2a7dea667a3fd1973a52fed6913
Sergio Martin <sergiom@eng.ucsd.edu>
2021-12-18 03:00:31 -0800

    Adding lvl8

--- source/frame.h
+++ source/frame.h
@@ -1,15 +1,15 @@
 #pragma once
 
 #ifndef _MAX_FRAME_DIFF
- #define _MAX_FRAME_DIFF 110
+ #define _MAX_FRAME_DIFF 90
 #endif
 
 #ifndef _MAX_RULE_COUNT
- #define _MAX_RULE_COUNT 26
+ #define _MAX_RULE_COUNT 18
 #endif
 
 #ifndef _MAX_MOVELIST_SIZE
- #define _MAX_MOVELIST_SIZE 935
+ #define _MAX_MOVELIST_SIZE 900
 #endif
 
 #define _MAX_MOVELIST_STORAGE ((_MAX_MOVELIST_SIZE/2) + 1)
